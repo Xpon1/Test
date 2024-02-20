a8778d5a0dffc52f226266124995416d619f92d0
Yasin Arafat <127176409+Xpon1@users.noreply.github.com>
2024-02-20 13:47:16 +0600

    Update YASIN.py

--- YASIN.py
+++ YASIN.py
@@ -7,7 +7,7 @@ except:os.system("pip uninstall requests -y;pip install requests")
  
 #print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
-ranaxd = platform.architecture()[0]
+xponxd = platform.architecture()[0]
 if xponxd == '64bit':
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
  import Xpon
